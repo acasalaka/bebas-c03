2c79828cad798476ef4afd3cc3247178cb39d701
adrasa.cantya <adrasa.cantya@ui.ac.id>
2024-12-22 02:22:38 +0700

    feat: initialization

---
+++ c03/src/main/java/elabsky/c03/C03Application.java
@@ -0,0 +1,13 @@
+package elabsky.c03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C03Application.class, args);
+	}
+
+}
---
+++ c03/src/test/java/elabsky/c03/C03ApplicationTests.java
@@ -0,0 +1,13 @@
+package elabsky.c03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
